e03bbe5a7f51510fac1af010b89d872db0a41f93
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-03-21 16:51:37 +0000

    Challenge 1 completed, create sidebar component and add links

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div>
+            <a href="#." target="_blank" alt="Link to my photos">MyPhotos</a>
+            <a href="#." target="_blank" alt="Link to my illustrations">My Illustrations</a>
+            <a href="#." target="_blank" alt="Link to my paintings">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar;
