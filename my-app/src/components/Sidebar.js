76dc23f4aa13f177fba8d90a9be60fbab8e6e1c8
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-03-21 17:20:16 +0000

    Challenge 2 completed, import and add css to sidebar component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,10 @@
 import React from "react";
+import css from './css/Sidebar.module.css';
+
 
 function Sidebar() {
     return (
-        <div>
+        <div className={css.sidebar}>
             <a href="#." target="_blank" alt="Link to my photos">MyPhotos</a>
             <a href="#." target="_blank" alt="Link to my illustrations">My Illustrations</a>
             <a href="#." target="_blank" alt="Link to my paintings">My Paintings</a>
